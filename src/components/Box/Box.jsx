9d7a4d1787163404ef771d4e9a283382b998c3f2
Alexander Khardin <khporter1984@gmail.com>
2022-09-27 16:52:12 +0300

    init project

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { layout, space, flexbox, color, borders } from 'styled-system';
+
+export const Box = styled.div`
+  ${layout}
+  ${flexbox}
+  ${space}
+  ${color}
+  ${borders}
+`;
